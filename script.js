4750c0bad26b04ec2c1d97061a60829690c6a832
Otene Victor <solomonotene36@gmail.com>
2022-11-01 23:40:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert('this is  opening of lundary installation')
+// let modal = document.getElementById('id01');
+
+// window.onclick = function(event) {
+//     If (event.target == modal) 
+//     {
+//      modal.style.display = "none";
+//     }
+// }
+
+// function openNav() {
+//     document.getElementById("myNav")
+//     .style.width ="100%";
+//    }
+   
+//    function closeNav() {
+//     document.getElementById("myNav")
+//     .style.width = "0%";
+//    }
